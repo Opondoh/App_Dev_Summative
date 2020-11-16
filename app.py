b3e47a6fae44dc9659ecb7f03ef68fae491e54f3
Viv-Iam <vivian.opondoh@gmail.com>
2020-11-16 05:21:45 +0300

    Create endpoint that allows upload of CSV file to change the maintenance schedule

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
-import pickle   
+from flask import Flask, request, render_template, make_response
+from flask_restful import Api, Resource, reqparse
+from werkzeug import secure_filename
+import pandas as pd
+import cgitb
+import json
+import csv
+import cgi
 
-# load the model from disk
-loaded_model = pickle.load(open(filename, 'rb'))
-result = loaded_model.score(X_test, Y_test)
-print(result)
+BASE = "http://127.0.0.1:5000/"
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/', methods=['GET'])
+def index():
+  header = {'Content-Type': 'text/html'}
+  return make_response(render_template('index.html'), 200, header)
+     
+@app.route('/uploadcsv', methods=['GET', 'POST'])
+def upload():
+  if request.method=='POST':
+    f = request.files['csvfile']
+    data = []
+    with open(f.filename) as file:
+      csvfile = csv.reader(file)
+      for row in csvfile:
+        data.append(row)
+      data = pd.DataFrame(data).to_html(header=False, index=False, classes=["table", "table-bordered", "table-dark text-center"])
+    return render_template('index.html', data=data)
+  else:
+    header = {'Content-Type': 'text/html'}
+    return make_response(render_template('index.html'), 200, header)
+
+if __name__ == "__main__":
+  app.run(debug=True)
